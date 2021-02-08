765163fcc0a2ecb19bf548fa265a9ae7143cc085
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2021-02-08 17:22:11 +0000

    Working with RDS database instead of local DB

--- classes/Connection.php
+++ classes/Connection.php
@@ -1,18 +1,19 @@
 <?php
-class Connection {
 
-    private static $host = "localhost";
-    private static $database = "cloud_festivals_db";
-    private static $username = "root";
-    private static $password = "";
+class Connection
+{
+    private static $host = 'festival-cloud-db.coqgvfohjb77.us-east-1.rds.amazonaws.com';
+    private static $database = 'festival_cloud_db';
+    private static $username = 'admin';
+    private static $password = 'secretRDS';
 
-    public static function getInstance() {
-        $dsn = 'mysql:host=' . Connection::$host . ';dbname=' . Connection::$database;
+    public static function getInstance()
+    {
+        $dsn = 'mysql:host='.Connection::$host.';dbname='.Connection::$database;
 
         $connection = new PDO($dsn, Connection::$username, Connection::$password);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         return $connection;
     }
-
 }
