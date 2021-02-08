c2e35c3de1b830e97eca09b20f2f0d43481daffb
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2021-02-08 15:42:20 +0000

    Images uploaded to s3 now displayed in frontend for festivals

--- classes/Festival.php
+++ classes/Festival.php
@@ -1,7 +1,12 @@
 <?php
+
 require_once 'Connection.php';
 
-class Festival {
+require_once 'StaticFile.php';
+use FestivalCloud\StaticFile;
+
+class Festival
+{
     public $id;
     public $title;
     public $description;
@@ -10,107 +15,110 @@ class Festival {
     public $end_date;
     public $image_path;
 
-    public function __construct() {
+    public function __construct()
+    {
     }
 
-    public function save() {
-        $params = array(
+    public function save()
+    {
+        $params = [
             'title' => $this->title,
             'description' => $this->description,
             'city' => $this->city,
             'start_date' => $this->start_date,
             'end_date' => $this->end_date,
-            'image_path' => $this->image_path
-        );
+            'image_path' => $this->image_path,
+        ];
 
-        if ($this->id === NULL) {
-            $sql = "INSERT INTO festivals(
+        if (null === $this->id) {
+            $sql = 'INSERT INTO festivals(
                         title, description, city, start_date, end_date, image_path
                     ) VALUES (
                         :title, :description, :city, :start_date, :end_date, :image_path
-                    )";
-        }
-        else if ($this->id !== NULL) {
-            $params["id"] = $this->id;
+                    )';
+        } elseif (null !== $this->id) {
+            $params['id'] = $this->id;
 
-            $sql = "UPDATE festivals SET
+            $sql = 'UPDATE festivals SET
                         title = :title,
                         description = :description,
                         city = :city,
                         start_date = :start_date,
                         end_date = :end_date,
                         image_path = :image_path
-                    WHERE id = :id";
+                    WHERE id = :id';
         }
 
         $conn = Connection::getInstance();
         $stmt = $conn->prepare($sql);
         $success = $stmt->execute($params);
         if (!$success) {
-            throw new Exception("Failed to save festival");
+            throw new Exception('Failed to save festival');
         }
-        else {
-            $rowCount = $stmt->rowCount();
-            if ($rowCount !== 1) {
-                throw new Exception("Error saving festival");
-            }
-            if ($this->id === NULL) {
-                $this->id = $conn->lastInsertId('festivals');
-            }
+
+        $rowCount = $stmt->rowCount();
+        if (1 !== $rowCount) {
+            throw new Exception('Error saving festival');
+        }
+        if (null === $this->id) {
+            $this->id = $conn->lastInsertId('festivals');
         }
     }
 
-    public function delete() {
+    public function delete()
+    {
         if (empty($this->id)) {
-            throw new Exception("Unsaved festival cannot be deleted");
+            throw new Exception('Unsaved festival cannot be deleted');
         }
-        $params = array(
-            'id' => $this->id
-        );
+        $params = [
+            'id' => $this->id,
+        ];
         $sql = 'DELETE FROM festivals WHERE id = :id';
         $connection = Connection::getInstance();
         $stmt = $connection->prepare($sql);
         $success = $stmt->execute($params);
         if (!$success) {
-            throw new Exception("Failed to delete festival");
+            throw new Exception('Failed to delete festival');
         }
-        else {
-            $rowCount = $stmt->rowCount();
-            if ($rowCount !== 1) {
-                throw new Exception("Error deleting festival");
-            }
+
+        $rowCount = $stmt->rowCount();
+        if (1 !== $rowCount) {
+            throw new Exception('Error deleting festival');
         }
     }
 
-    public static function all() {
+    public static function all()
+    {
         $sql = 'SELECT * FROM festivals';
         $connection = Connection::getInstance();
         $stmt = $connection->prepare($sql);
         $success = $stmt->execute();
         if (!$success) {
-            throw new Exception("Failed to retrieve festivals");
+            throw new Exception('Failed to retrieve festivals');
         }
-        else {
-            $festivals = $stmt->fetchAll(PDO::FETCH_CLASS, 'Festival');
-            return $festivals;
-        }
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS, 'Festival');
     }
 
-    public static function find($id) {
-        $params = array(
-            'id' => $id
-        );
+    public static function find($id)
+    {
+        $params = [
+            'id' => $id,
+        ];
         $sql = 'SELECT * FROM festivals WHERE id = :id';
         $connection = Connection::getInstance();
         $stmt = $connection->prepare($sql);
         $success = $stmt->execute($params);
         if (!$success) {
-            throw new Exception("Failed to retrieve festival");
+            throw new Exception('Failed to retrieve festival');
         }
-        else {
-            $festival = $stmt->fetchObject('Festival');
-            return $festival;
+
+        $festival = $stmt->fetchObject('Festival');
+        if (!strpos($festival->image_path, 'placeimg')) {
+            $files = new StaticFile();
+            $festival->image_path = $files->getFileLink($festival->image_path);
         }
+
+        return $festival;
     }
 }
-?>
--- views/festivals/show.php
+++ views/festivals/show.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../../classes/Festival.php';
+
 require_once '../../classes/Gump.php';
 
 try {
@@ -7,36 +8,34 @@ try {
 
     $_GET = $validator->sanitize($_GET);
 
-    $validation_rules = array(
-        'id' => 'required|integer|min_numeric,1'
-    );
-    $filter_rules = array(
-    	'id' => 'trim|sanitize_numbers'
-    );
+    $validation_rules = [
+        'id' => 'required|integer|min_numeric,1',
+    ];
+    $filter_rules = [
+        'id' => 'trim|sanitize_numbers',
+    ];
 
     $validator->validation_rules($validation_rules);
     $validator->filter_rules($filter_rules);
 
     $validated_data = $validator->run($_GET);
 
-    if($validated_data === false) {
+    if (false === $validated_data) {
         $errors = $validator->get_errors_array();
-        throw new Exception("Invalid festival id: " . $errors['id']);
+
+        throw new Exception('Invalid festival id: '.$errors['id']);
     }
 
     $id = $validated_data['id'];
     $festival = Festival::find($id);
-    
+
     $img_src = $festival->image_path;
-    
-    if(!strpos($img_src, 'placeimg')) {
-      $img_src = "../../" . $festival->image_path;
-    }
-    
-    
-}
-catch (Exception $ex) {
-    die($ex->getMessage());
+
+    // if(!strpos($img_src, 'placeimg')) {
+    //   $img_src = "../../" . $festival->image_path;
+    // }
+} catch (Exception $ex) {
+    exit($ex->getMessage());
 }
 ?>
 <!DOCTYPE html>
@@ -61,34 +60,34 @@ catch (Exception $ex) {
                     <table id="table-festival" class="table table-hover">
                         <tbody>
                             <tr>
-                                <td><img src="<?= $img_src ?>" height="200px" /></td>
+                                <td><img src="<?php echo $img_src; ?>" height="200px" /></td>
                             </tr>
                             <tr>
                                 <td width="20%">Title</td>
-                                <td><?= $festival->title ?></td>
+                                <td><?php echo $festival->title; ?></td>
                             </tr>
                             <tr>
                                 <td>Description</td>
-                                <td><?= $festival->description ?></td>
+                                <td><?php echo $festival->description; ?></td>
                             </tr>
                             <tr>
                                 <td>City</td>
-                                <td><?= $festival->city ?></td>
+                                <td><?php echo $festival->city; ?></td>
                             </tr>
                             <tr>
                                 <td>Start</td>
-                                <td><?= $festival->start_date ?></td>
+                                <td><?php echo $festival->start_date; ?></td>
                             </tr>
                             <tr>
                                 <td>End</td>
-                                <td><?= $festival->end_date ?></td>
+                                <td><?php echo $festival->end_date; ?></td>
                             </tr>
                         </tbody>
                     </table>
                     <p>
                         <a href="index.php" class="btn btn-default">Cancel</a>
-                        <a href="edit.php?id=<?= $festival->id ?>" class="btn btn-warning">Edit</a>
-                        <a href="delete.php?id=<?= $festival->id ?>" class="btn btn-danger">Delete</a>
+                        <a href="edit.php?id=<?php echo $festival->id; ?>" class="btn btn-warning">Edit</a>
+                        <a href="delete.php?id=<?php echo $festival->id; ?>" class="btn btn-danger">Delete</a>
                     </p>
                 </div>
             </div>
